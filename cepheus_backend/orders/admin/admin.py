15833acf0f2930db10d677940a4d097cdf8a6e3e
mhvozdesky <m.hvozdesky@gmail.com>
2023-08-09 14:58:25 +0300

    B18. OrderGood inlines in OrderAdmin

--- cepheus_backend/orders/admin/admin.py
+++ cepheus_backend/orders/admin/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from ..models import Customer, Order, Category, Good
+from ..models import Customer, Order, Category, Good, OrderGood
+
+
+class OrderGoodInline(admin.TabularInline):
+    model = OrderGood
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderGoodInline]
 
 
 admin.site.register(Customer)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Category)
 admin.site.register(Good)
