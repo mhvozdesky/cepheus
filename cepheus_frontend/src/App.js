b58cdea1fe264414946b902b992ef790b88ba8bb
mhvozdesky <m.hvozdesky@gmail.com>
2023-08-14 19:16:15 +0300

    B24. implemented route to fictitious login and orders pages

--- cepheus_frontend/src/App.js
+++ cepheus_frontend/src/App.js
@@ -1,11 +1,27 @@
+import React, {useState, useEffect} from "react";
 import './styles/App.css';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter"
+import {AuthContex} from "./contex/index"
 
 function App() {
+  const [isAuth, setIsAuth] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setIsAuth(true)
+    }
+    setIsLoading(false)
+  }, [])
+
   return (
-    <div className="App">
-      spam
-    </div>
-  );
+    <AuthContex.Provider value={{isAuth, setIsAuth, isLoading}}>
+      <BrowserRouter>
+        <AppRouter/>
+      </BrowserRouter>
+    </AuthContex.Provider>
+  )
 }
 
 export default App;
---
+++ cepheus_frontend/src/pages/OrdersPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrdersPage = function() {
+    return (
+        <div>
+            Orders page
+        </div>
+    );
+};
+
+export default OrdersPage;
