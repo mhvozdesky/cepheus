25ec825f7c91337177305ffc67f7be6386cb5d22
mhvozdesky <m.hvozdesky@gmail.com>
2023-08-16 13:25:49 +0300

    B26. Made three selects in the orders command panel

--- cepheus_frontend/src/pages/OrdersPage.jsx
+++ cepheus_frontend/src/pages/OrdersPage.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import ButtonAdd from "../components/UI/ButtonAdd"
+import SelectOption from "../components/UI/SelectOption"
 
 const OrdersPage = function() {
     return (
         <div className='page order-page'>
             <div className='page-header'>
                 <ButtonAdd />
+                <SelectOption
+                    class_name='responsible'
+                    defaultValue="Відповідальний"
+                    options={[
+                        {value: 1, name: "Антон Сурін"},
+                        {value: 2, name: "Аліса Срібчук"}
+                    ]}
+                />
+                <SelectOption
+                    class_name='status'
+                    defaultValue="Статус"
+                    options={[
+                        {value: 'in_progress', name: "В роботі"},
+                        {value: 'canceled', name: "Анульовано"},
+                        {value: 'returned', name: "Повернено"},
+                        {value: 'shipped', name: "В дорозі"},
+                        {value: 'shipped_back', name: "В дорозі назад"},
+                        {value: 'completed', name: "Готово"},
+                    ]}
+                />
+                <SelectOption
+                    class_name='date'
+                    defaultValue="Період"
+                    options={[
+                    ]}
+                />
             </div>
             <div className='page-content'></div>
         </div>
