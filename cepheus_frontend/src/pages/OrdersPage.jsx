a7877e2abab136e3289ffe7ff197a829b7672afc
mhvozdesky <m.hvozdesky@gmail.com>
2023-08-16 12:48:16 +0300

    B26. Added an add button to the orders command panel

--- cepheus_frontend/src/pages/OrdersPage.jsx
+++ cepheus_frontend/src/pages/OrdersPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ButtonAdd from "../components/UI/ButtonAdd"
 
 const OrdersPage = function() {
     return (
-        <div className='page'>
-            Orders page
+        <div className='page order-page'>
+            <div className='page-header'>
+                <ButtonAdd />
+            </div>
+            <div className='page-content'></div>
         </div>
     );
 };
