6b81ea3ff59975f6fead2ea5b19a1c6fd21f5c5c
mhvozdesky <m.hvozdesky@gmail.com>
2023-08-30 15:53:51 +0300

    B32. Implemented pagination functionality

--- cepheus_frontend/src/components/UI/SelectOption.jsx
+++ cepheus_frontend/src/components/UI/SelectOption.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const SelectOption = function({class_name, options, defaultValue, value}) {
-    const element_class = 'ceph-select' + ' ' + class_name;
+const SelectOption = function(props) {
+    const element_class = 'ceph-select' + ' ' + props.class_name;
+
+    const changeHandle = (e) => {
+        if (props.change_page_size) {
+            props.change_page_size(e.target.value)
+        }
+    }
+
     return (
-        <select className={element_class}>
-            <option value="">--{defaultValue}--</option>
-            {options.map(option => 
+        <select className={element_class} value={props.pageSize} onChange={changeHandle} id={props.id}>
+            {props.defaultValue &&
+                <option value="">--{props.defaultValue}--</option>
+            }
+            {props.options.map(option => 
                 <option key={option.value} value={option.value}>
                     {option.name}
                 </option>    
