d356a4550a29c8674d0baea94ac1296651c84563
mhvozdesky <m.hvozdesky@gmail.com>
2023-09-08 18:17:36 +0300

    B39. you can disable control buttons for LabeledInput

--- cepheus_frontend/src/components/UI/LabeledInput.jsx
+++ cepheus_frontend/src/components/UI/LabeledInput.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 const LabeledInput = function(props) {
+    var mainClassName = `labeled-input ${props.input_name}`
+
+    if (props.control_elem) {
+        mainClassName = mainClassName + ' is-control-elem'
+    }
+
     return (
-        <div className={`labeled-input ${props.input_name}`}>
+        <div className={mainClassName}>
             <label htmlFor={props.input_name}>{props.label_text}</label>
             <div className='content-block'>
                 <div className='text-wrapper'>
-                    <input type="text" id={props.input_name} name={props.input_name} value={props.value} onChange={(e) => props.change(e.target.value)} />
+                    <input
+                        type="text"
+                        id={props.input_name}
+                        name={props.input_name}
+                        value={props.value}
+                        onChange={(e) => props.change(e.target.id, e.target.value)}
+                        readOnly={props.readOnly}
+                    />
                 </div>
-                <div className='list-btn'></div>
-                <div className='detail-btn'></div>
+                {props.control_elem &&
+                    <>
+                        <div className='list-btn'></div>
+                        <div className='detail-btn'></div>
+                    </>
+                }
             </div>
         </div>
     );
