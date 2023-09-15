9c529923ccedcdaaec8a8f1a11d7d083db1e57ba
mhvozdesky <m.hvozdesky@gmail.com>
2023-09-15 19:31:45 +0300

    B42. Added side-employees button. The EmployeesList component is created

---
+++ cepheus_frontend/src/pages/EmployeesList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeesList = function() {
+    return (
+        <div className='employees-list'>
+            spam
+        </div>
+    );
+};
+
+export default EmployeesList;
