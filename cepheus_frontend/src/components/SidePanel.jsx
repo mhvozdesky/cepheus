b1ab42b3ed3e54b78e03e3edd3dee434a9e778e9
mhvozdesky <m.hvozdesky@gmail.com>
2023-09-19 15:21:01 +0300

    B46. Highlight the active section in the sidebar

--- cepheus_frontend/src/components/SidePanel.jsx
+++ cepheus_frontend/src/components/SidePanel.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ItemSidePanel from "./ItemSidePanel"
+import {useLocation} from "react-router-dom"
 
 const SidePanel = function() {
+    const location = useLocation();
+
     return (
         <div className='side-panel'>
             <div className='blank-block'></div>
-            <ItemSidePanel class_name='side-orders' title='Замовлення' url='orders' />
-            <ItemSidePanel class_name='side-goods' title='Товари' url='goods' />
-            <ItemSidePanel class_name='side-employees' title='Команда' url='employees' />
-            <ItemSidePanel class_name='side-customers' title='Покупці' url='customers' />
-            <ItemSidePanel class_name='side-categories' title='Категорії' url='categories' />
+            <ItemSidePanel class_name='side-orders' title='Замовлення' url='orders' active={location.pathname.startsWith('/orders')} />
+            <ItemSidePanel class_name='side-goods' title='Товари' url='goods' active={location.pathname.startsWith('/goods')} />
+            <ItemSidePanel class_name='side-employees' title='Команда' url='employees' active={location.pathname.startsWith('/employees')} />
+            <ItemSidePanel class_name='side-customers' title='Покупці' url='customers' active={location.pathname.startsWith('/customers')} />
+            <ItemSidePanel class_name='side-categories' title='Категорії' url='categories' active={location.pathname.startsWith('/categories')} />
         </div>
     );
 };
