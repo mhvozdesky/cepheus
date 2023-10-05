962de99a03a073567ef0c675441a84902aab5be0
mhvozdesky <m.hvozdesky@gmail.com>
2023-10-05 15:09:41 +0300

    B54. Goods filtering is implemented, we get a list of field values

--- cepheus_backend/orders/filters.py
+++ cepheus_backend/orders/filters.py
@@ -1,28 +1,19 @@
-import base64
-import json
-
 import django_filters
 
 from django_filters import rest_framework as filters
-from django.db.models import Q
 
 from .models import Good
+from .utils import filter_list_method
 
 
 class GoodFilters(filters.FilterSet):
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', method='title_filter_method')
-    vendor = django_filters.CharFilter(field_name='vendor_code', lookup_expr='icontains')
+    id = django_filters.CharFilter(method='filter_method')
+    title = django_filters.CharFilter(lookup_expr='icontains', method='filter_method')
+    vendor_code = django_filters.CharFilter(field_name='vendor_code', lookup_expr='icontains', method='filter_method')
 
     class Meta:
         model = Good
-        fields = ('id', 'title', 'vendor')
+        fields = []
 
-    def title_filter_method(self, queryset, name, value):
-        decoded_bytes = base64.b64decode(value)
-        decoded_string = decoded_bytes.decode('utf-8')
-        value_list = json.loads(decoded_string)
-
-        q_objects = Q()
-        for value in value_list:
-            q_objects |= Q(title__icontains=value)
-        return queryset.filter(q_objects)
+    def filter_method(self, queryset, name, value):
+        return filter_list_method(self, queryset, name, value)
