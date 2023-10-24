1b941b3bf64ea969e79ad4c48f24cb759e383836
mhvozdesky <m.hvozdesky@gmail.com>
2023-10-24 19:14:16 +0300

    B58. implemented search by order on the backend

--- cepheus_backend/orders/filters.py
+++ cepheus_backend/orders/filters.py
@@ -1,8 +1,11 @@
 import django_filters
 
+from django.db.models import Value as V, F
+from django.db.models.functions import Concat
+
 from django_filters import rest_framework as filters
 
-from .models import Good
+from .models import Good, Order
 from .utils import filter_list_method
 
 
@@ -17,3 +20,24 @@ class GoodFilters(filters.FilterSet):
 
     def filter_method(self, queryset, name, value):
         return filter_list_method(self, queryset, name, value)
+
+
+class OrderFilters(filters.FilterSet):
+    id = django_filters.CharFilter(method='text_search_method')
+    responsible_full_name = django_filters.CharFilter(lookup_expr='icontains', method='text_search_method')
+    customer_email = django_filters.CharFilter(lookup_expr='icontains', method='text_search_method')
+
+    class Meta:
+        model = Order
+        fields = []
+
+    @staticmethod
+    def _get_extended_queryset(queryset):
+        return queryset.annotate(
+            responsible_full_name=Concat('responsible__first_name', V(' '), 'responsible__last_name'),
+            customer_email=F('customer__email')
+        )
+
+    def text_search_method(self, queryset, name, value):
+        qs = self._get_extended_queryset(queryset)
+        return filter_list_method(self, qs, name, value)
